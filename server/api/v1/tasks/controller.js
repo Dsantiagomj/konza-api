ffe56c7fb32947f60abbdb111f07e7d0f12abb3d
Daniel Santiago Muñoz Jaramillo <dsantiagomj@gmail.com>
2019-08-05 19:55:02 -0500

    crud controller

--- server/api/v1/tasks/controller.js
+++ server/api/v1/tasks/controller.js
@@ -1,35 +1,213 @@
+const uuidv1 = require('uuid/v1');
+
+const tasks = [
+  {
+    _id: uuidv1(),
+    title: 'Buy Milk',
+    description: 'For breakfast',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+  {
+    _id: uuidv1(),
+    title: 'Buy Meat',
+    description: 'For lunch',
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  },
+];
+
 exports.create = (req, res, next) => {
   const { body = {} } = req;
-  res.json(body);
+  console.log(body);
+  const { title = '', description = '' } = body;
+  tasks.push({
+    _id: uuidv1(),
+    title,
+    description,
+    created: new Date().toISOString(),
+    updated: new Date().toISOString(),
+  });
+  res.json(tasks);
 };
 
 exports.all = (req, res, next) => {
-  res.json([]);
+  const { query } = req;
+  const {
+    limit = 10, skip = 0, sortBy = 'created', direction = 'desc',
+  } = query;
+  const sortOrder = direction === 'desc' ? -1 : 1;
+  const tasksClone = new Array(...tasks);
+  const sortedTasks = tasksClone.sort((a, b) => {
+    if (a[sortBy] > b[sortBy]) {
+      return 1 * sortOrder;
+    }
+    if (a[sortBy] < b[sortBy]) {
+      return -1 * sortOrder;
+    }
+    return 0;
+  });
+
+  const tasksLimited = sortedTasks.slice(Number(skip), Number(limit) + Number(skip));
+
+  return res.json(tasksLimited);
 };
 
 exports.read = (req, res, next) => {
   const { params = {} } = req;
   const { id = 1 } = params;
 
-  res.json({
-    id,
-  });
+  const selectedTask = tasks.find(task => task._id === id) || {};
+
+  res.json(selectedTask);
 };
 
 exports.update = (req, res, next) => {
-  const { params = {}, body = {} } = req;
+  const { body = {}, params } = req;
+  const { title = '', description = '' } = body;
   const { id = 1 } = params;
 
-  res.json({
-    id,
-    ...body,
-  });
+  const updateTask = tasks.find(task => task._id === id);
+  const newTask = {
+    title,
+    description,
+    updated: new Date().toISOString(),
+  };
+  Object.assign(updateTask, newTask);
+  res.json(tasks);
 };
 
 exports.delete = (req, res, next) => {
   const { params = {} } = req;
   const { id = 1 } = params;
-  res.json({
-    id,
-  });
+  const deleteTask = tasks.find(task => task._id === id) || {};
+  const deleteTaskIndex = tasks.indexOf(deleteTask);
+  tasks.splice(deleteTaskIndex, 1);
+  res.json(deleteTask);
 };
