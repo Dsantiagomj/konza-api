57bf5ded51cbb97595ea6c20b7c52aff3dd17a68
Daniel Santiago Muñoz Jaramillo <dsantiagomj@gmail.com>
2019-08-13 20:11:29 -0500

    Add config for populate Virtuals

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,4 +1,4 @@
-const { pagination, sort } = require('./../config');
+const { pagination, sort, populate } = require('./../config');
 
 const paginationParseParams = ({
   limit = pagination.limit,
@@ -30,11 +30,11 @@ const sortCompactToStr = (sortBy, direction) => {
   return `${dir}${sortBy}`;
 };
 
-// params = { userId: 'assafasfafs' }
+// params = { userId: '1234-abcd-5678' }
 // paramsNames = ['userId']
 // referencesNames ['userId', 'projectId']
 
-// filters: { userId: 'assafasfafs' }
+// filters: { userId: '1234-abcd-5678' }
 // populate: ['projectId']
 
 const filterByNested = (params, referencesNames) => {
@@ -46,9 +46,32 @@ const filterByNested = (params, referencesNames) => {
   };
 };
 
+// populateNames = ['userId', 'tasks']
+// virtuals = { tasks: ..... }
+// virtualNames = ['tasks']
+
+const populateToObject = (populateNames, virtuals = {}) => {
+  const virtualNames = Object.getOwnPropertyNames(virtuals);
+  const { virtuals: virtualConfig } = populate;
+  return populateNames.map((item) => {
+    let options = {};
+    if (virtualNames.includes(item)) {
+      options = {
+        limit: virtualConfig.limit,
+        sort: sortCompactToStr(virtualConfig.sort, virtualConfig.direction),
+      };
+    }
+    return {
+      path: item,
+      options,
+    };
+  });
+};
+
 module.exports = {
   paginationParseParams,
   sortParseParams,
   sortCompactToStr,
   filterByNested,
+  populateToObject,
 };
