6db14a67b9f853afad5027ce104743c563a8faba
Daniel Santiago Muñoz Jaramillo <dsantiagomj@gmail.com>
2019-08-15 18:46:06 -0500

    Add Auth Middleware

--- server/api/v1/projects/routes.js
+++ server/api/v1/projects/routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+
 const controller = require('./controller');
 const tasksRouter = require('./../tasks/routes');
+const { auth } = require('./../auth');
 
 /*
  * /api/tasks/ POST - CREATE
@@ -14,14 +16,14 @@ router.param('id', controller.id);
 
 router
   .route('/')
-  .post(controller.create)
-  .get(controller.all);
+  .post(auth, controller.create)
+  .get(auth, controller.all);
 
 router
   .route('/:id')
-  .get(controller.read)
-  .put(controller.update)
-  .delete(controller.delete);
+  .get(auth, controller.read)
+  .put(auth, controller.update)
+  .delete(auth, controller.delete);
 
 /*
  * /api/users/:projectId/tasks/ POST - CREATE
