fd50c9a8e1bbcc7f496fece54f85cc1e848e8ea4
Daniel Santiago Muñoz Jaramillo <dsantiagomj@gmail.com>
2019-08-15 20:04:23 -0500

    Add Custom Validator for Email and ProfilePhoto

--- server/api/v1/users/model.js
+++ server/api/v1/users/model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { hash, compare } = require('bcryptjs');
+const { isEmail, isURL } = require('validator');
 
 const { Schema } = mongoose;
 
@@ -21,6 +22,14 @@ const fields = {
     required: true,
     trim: true,
     unique: true,
+    validate: {
+      validator(value) {
+        return isEmail(value);
+      },
+      message(props) {
+        return `${props.value} is not a valid Email`;
+      },
+    },
   },
   password: {
     type: String,
@@ -30,7 +39,15 @@ const fields = {
   profilePhoto: {
     type: String,
     trim: true,
-    default: '',
+    required: true,
+    validate: {
+      validator(value) {
+        return isURL(value);
+      },
+      message(props) {
+        return `${props.value} is not a URL`;
+      },
+    },
   },
 };
 
@@ -81,9 +98,8 @@ user.pre('save', async function save(next) {
 });
 
 // prettier-ignore
-user.methods.verifyPassword = async function verifyPassword(password) {
-  const result = compare(password, this.password);
-  return result;
+user.methods.verifyPassword = function verifyPassword(password) {
+  return compare(password, this.password);
 };
 
 module.exports = {
